e0b77b2418c52bc5402463aa21eee7975a9b3889
Kunal Thakkar <kunalmthakkar@gmail.com>
2023-03-19 14:39:03 +0530

    Added code for d1_mini read raw values

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Check branch for specific mirco-controller implementation
+To get started with D1 Mini check 
+https://github.com/kunal-thakkar/embedded-BlinkLed/tree/d1_mini
+
+# MAX30102 with D1 MINI
+
+
+## Install the libraries.
+* Tools > Manage Libraries > search for MAX30300
+* Install “SparkFun_MAX3010x_Sensor_Library”
+* Use Tools > Serial Monitor to watch what happens * with the code [set to 115200 baud!!!!]
+* Libraries are found here:
+`C:\Users\myUserName\Documents\Arduino\libraries`
---
+++ max30102.ino
@@ -0,0 +1,75 @@
+/*
+  MAX30105 Breakout: Output all the raw Red/IR/Green readings
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 2nd, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
+
+  Outputs all Red/IR/Green values.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+
+  This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+*/
+
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+
+#define debug Serial //Uncomment this line if you're using an Uno or ESP
+//#define debug SerialUSB //Uncomment this line if you're using a SAMD21
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+
+void setup()
+{
+  debug.begin(9600);
+  debug.println("MAX30105 Basic Readings Example");
+
+  // Initialize sensor
+  if (particleSensor.begin() == false)
+  // if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
+  {
+    debug.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+
+  particleSensor.setup(); //Configure sensor with default settings
+
+  //The LEDs are very low power and won't affect the temp reading much but
+  //you may want to turn off the LEDs to avoid any local heating
+  // particleSensor.setup(0); //Configure sensor. Turn off LEDs
+  //particleSensor.setup(); //Configure sensor. Use 25mA for LED drive
+
+  particleSensor.enableDIETEMPRDY(); //Enable the temp ready interrupt. This is required.
+
+}
+
+void loop()
+{
+  debug.print(" R[");
+  debug.print(particleSensor.getRed());
+  debug.print("] IR[");
+  debug.print(particleSensor.getIR());
+  debug.print("] G[");
+  debug.print(particleSensor.getGreen());
+  debug.print("] T[");
+  debug.print(particleSensor.readTemperatureF(), 4);
+  debug.print("]");
+  debug.println();
+}
